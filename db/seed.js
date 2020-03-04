996523bd1c84a5ea32195226833f3f0fc33b77b8
Joshua Reed <joshualreed93@gmail.com>
2020-03-04 08:42:42 -0500

    seed

--- db/seed.js
+++ db/seed.js
@@ -1,2 +1,7 @@
 const data = require('./csvjson.json')
 const recipe = require('../models/Recipes')
+
+recipe.remove({})
+recipe.create(data).then(recipes=>{
+    console.log(recipes)
+})
--- models/Recipes.js
+++ models/Recipes.js
@@ -1,2 +1,25 @@
 const mongoose = require('../db/connection')
 const Schema = mongoose.Schema;
+
+const recipes = new Schema({
+    title: String,
+    pricePerServing: Number,
+    weightPerServing: Number,
+    vegetarian: Boolean,
+    veryPopular: Boolean,
+    readyInMinutes: Number,
+    spoonacularSourceUrl: String,
+    image: String,
+    dishTypes: String,
+    ingredients: String,
+    calories: Number,
+    ["Fat/g"]: Number,
+    ["Saturated-Fat/g"]: Number,
+    ["Carbohydrates/g"]: Number,
+    ["Sugar/g"]: Number,
+    ["Cholesterol/mg"]: Number,
+    ["Sodium/mg"]: Number,
+    ["Protein/g"]: Number,
+})
+
+module.exports = mongoose.model('Recipes', recipes)
